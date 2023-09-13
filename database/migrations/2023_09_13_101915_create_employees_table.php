d0618376aaf20c4fc38e8b497d0489a33f1edf8e
Bra-Gabby-Marshal <sodeygabriel123@gmail.com>
2023-09-13 10:34:38 +0000

    added the attributes to the migrations

--- database/migrations/2023_09_13_101915_create_employees_table.php
+++ database/migrations/2023_09_13_101915_create_employees_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,16 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('country_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('state_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('department_id')->constrained()->cascadeOnDelete();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('address');
+            $table->char('zip_code');
+            $table->date('birth_date');
+            $table->date('date_hired');
             $table->timestamps();
         });
     }
